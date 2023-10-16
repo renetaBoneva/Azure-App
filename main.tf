9cf740c7fce146662c1019b933ae9a9b30d25cfd
Ренета Бонева <reneta.boneva@abv.bg>
2023-10-16 23:11:17 +0300

    Update files

--- main.tf
+++ main.tf
@@ -2,74 +2,72 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "3.75.0"
     }
+  }
+  backend "azurerm" {
+    resource_group_name  = "StorageRG"
+    storage_account_name = "taskboardstoragedimo5"
+    container_name       = "taskboardcontainer"
+    key                  = "terraform.tfstate"
   }
 }
 
 provider "azurerm" {
-  #   skip_provider_registration = true
-  #    # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
 
-# Generate a random integer to create a globally unique name
 resource "random_integer" "ri" {
   min = 10000
   max = 99999
 }
 
-# Create a resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource_group_name}${random_integer.ri.result}"
+  #name     = "${var.resource_group_name}${random_integer.ri.result}"
+  name     = var.resource_group_name
   location = var.resource_group_location
 }
 
-# Create the Linux App Service Plan
-resource "azurerm_service_plan" "servicePlan" {
-  name                ="${var.app_service_plan_name}${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
+resource "azurerm_service_plan" "asp" {
+  name                = var.app_service_plan_name
   resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
   sku_name            = "F1"
 }
 
-# Create the web app, pass in the App Service Plan ID
-resource "azurerm_linux_web_app" "linuxWebApp" {
-  name                = "${var.app_service_name}${random_integer.ri.result}"
+resource "azurerm_linux_web_app" "alwa" {
+  name                = var.app_service_name
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_service_plan.servicePlan.location
-  service_plan_id     = azurerm_service_plan.servicePlan.id
+  location            = azurerm_service_plan.asp.location
+  service_plan_id     = azurerm_service_plan.asp.id
 
-  # Add site configurations 
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.database.name};User ID=${azurerm_mssql_server.sqlserver.administrator_login};Password=${azurerm_mssql_server.sqlserver.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+  }
+
   site_config {
     application_stack {
       dotnet_version = "6.0"
     }
     always_on = false
   }
-
-  connection_string {
-    name  = "DefaultConnection"
-    type  = "SQLAzure"
-    value = "Data Source=tcp:${azurerm_mssql_server.dbServer.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.database.name};User ID=${azurerm_mssql_server.dbServer.administrator_login};Password=${azurerm_mssql_server.dbServer.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
-  }
 }
 
-resource "azurerm_mssql_server" "dbServer" {
-  name                = "${var.sql_server_name}${random_integer.ri.result}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  version             = "12.0"
-  # administrator_login          = "missadministrator"
-  # administrator_login_password = "thisIsKat11"
-  administrator_login          = var.sql_admin_login
-  administrator_login_password = var.sql_admin_password
+resource "azurerm_mssql_server" "sqlserver" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = var.sql_administrator_login_username
+  administrator_login_password = var.sql_administrator_password
 }
 
 resource "azurerm_mssql_database" "database" {
-  name           = "${var.sql_database_name}${random_integer.ri.result}"
-  server_id      = azurerm_mssql_server.dbServer.id
+  name           = var.sql_database_name
+  server_id      = azurerm_mssql_server.sqlserver.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
   max_size_gb    = 2
@@ -78,16 +76,15 @@ resource "azurerm_mssql_database" "database" {
 }
 
 resource "azurerm_mssql_firewall_rule" "firewall" {
-  name             = "${var.firewall_rule_name}${random_integer.ri.result}"
-  server_id        = azurerm_mssql_server.dbServer.id
+  name             = var.firewall_rule_name
+  server_id        = azurerm_mssql_server.sqlserver.id
   start_ip_address = "0.0.0.0"
   end_ip_address   = "0.0.0.0"
 }
 
-# Deploy code from public GitHub repo
 resource "azurerm_app_service_source_control" "example" {
-  app_id                 = azurerm_linux_web_app.linuxWebApp.id
-  repo_url               = var.repo_url
+  app_id                 = azurerm_linux_web_app.alwa.id
+  repo_url               = "https://github.com/dimosoftuni/taskboard"
   branch                 = "main"
   use_manual_integration = true
 }
--- variables.tf
+++ variables.tf
@@ -1,49 +1,49 @@
 variable "resource_group_name" {
-  type = string
-  description = "Resource group name in Azure"
+  type        = string
+  description = "The resource group name"
 }
 
 variable "resource_group_location" {
-  type = string
-  description = "Resource group location in Azure"
+  type        = string
+  description = "The resource group name"
 }
 
 variable "app_service_plan_name" {
-  type = string
-  description = "App service plan"
+  type        = string
+  description = "The resource group name"
 }
 
 variable "app_service_name" {
-  type = string
-  description = "App service name"
+  type        = string
+  description = "The resource group name"
 }
 
 variable "sql_server_name" {
-  type = string
-  description = "SQL server name"
+  type        = string
+  description = "The resource group name"
 }
 
 variable "sql_database_name" {
-  type = string
-  description = "SQL database name"
+  type        = string
+  description = "The resource group name"
 }
 
-variable "sql_admin_login" {
-  type = string
-  description = "SQL admin login username"
+variable "sql_administrator_login_username" {
+  type        = string
+  description = "The resource group name"
 }
 
-variable "sql_admin_password" {
-  type = string
-  description = "SQL admin login password"
+variable "sql_administrator_password" {
+  type        = string
+  description = "The resource group name"
 }
 
 variable "firewall_rule_name" {
-    type = string
-    description = "Firewall rule name"
+  type        = string
+  description = "The resource group name"
 }
 
-variable "repo_url" {
-    type = string
-    description = "Repo URL"  
+variable "repo_URL" {
+  type        = string
+  description = "Repo URL"
 }
